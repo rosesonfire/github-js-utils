991c39c49859eabfaafea5c043d36fca1963ca6e
Ishtiaque Khan <ishti.ik@gmail.com>
2018-03-26 21:38:15 +0600

    add iocHelper to src/index.js

--- src/main/index.js
+++ src/main/index.js
@@ -1,7 +1,9 @@
 import createDefensivePromise from './utils/createDefensivePromise'
 import foldLeft from './utils/foldLeft'
 import groupArrayItems from './utils/groupArrayItems'
+import iocHelper from './utils/iocHelper'
 import axiosWrapper from './wrappers/axiosWrapper'
 
-export const utils = { createDefensivePromise, foldLeft, groupArrayItems }
+export const utils =
+  { createDefensivePromise, foldLeft, groupArrayItems, iocHelper }
 export const wrappers = { axiosWrapper }
